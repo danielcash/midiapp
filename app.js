19b48c7bc7de96ebddd6c544f483c9b57220efe8
root <root@ip-172-31-22-52.ec2.internal>
2014-04-10 02:19:11 +0000

    added MIDI

--- app.js
+++ app.js
@@ -12,26 +12,48 @@ app.get("/", function(req, res) {
 server.listen(8080);
 
 var usernames = {};
+var messages = [];
 
 io.sockets.on('connection', function(socket) {
 	// when the client emits sendchat, this listens and executes
 	socket.on('sendchat', function(data) {
 		// tell the client to execute updatechat with 2 paramaters
 		io.sockets.emit('updatechat', socket.username, data);
+
+		// store the last ten messages
+		if (messages.length == 10)
+		{
+			messages.shift();
+		}
+		messages.push([socket.username, data]);
 	});
 
 	// when the client emits adduser this listen and executes
 	socket.on('adduser', function(username) {
+		if (usernames[username] != undefined)
+		{
+			socket.emit('askagain');
+		}
+		else
+		{
 		// we store the username in the socket session for this client
 		socket.username = username;
 		// add username to global list
 		usernames[username] = username;
 		// echo to client that they've connected
 		socket.emit('updatechat', 'SERVER', 'you have connected');
+
+		// send new user the last 10 messages
+		messages.forEach(function(msg)
+		{
+			socket.emit('updatechat', msg[0], msg[1], msg[2]);
+		});
+
 		// echo globally that a person has connected
 		socket.broadcast.emit('updatechat', 'SERVER', username + ' connected');
 		// update the list of users in chat,client side
 		io.sockets.emit('updateusers', usernames);
+		}
 	});
 
 	// when the user disconnects
@@ -48,6 +70,5 @@ io.sockets.on('connection', function(socket) {
 	socket.on('sendNote', function(data) {
 		// broadcast MIDI event to all users
 		io.sockets.emit('playnote', socket.username, data);
-		//socket.broadcast.emit('playnote', socket.username, ev);
 	});
 });
